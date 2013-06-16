25f233fa876c857d6d7c78dc0502a4c24710935c
Ting-Yen LAI <banacorn@gmail.com>
2013-06-16 17:49:25 +0800

    data Stat & instance as Show and Monoid

--- game/type.hs
+++ game/type.hs
@@ -1,5 +1,6 @@
 module Game.Type where
 
+import Data.Monoid
 import Data.List (intersperse)
 
  --datatypes
@@ -8,6 +9,12 @@ type Position = (Int, Int)
 type ActionSequence = [Position]
 data Game = Game [Chess] deriving (Eq)
 data Player = Player deriving (Eq)
+data Stat = Stat {
+    scoreFour :: Int,
+    openThree :: Int,
+    cornerAndCore :: Int,
+    surface :: Int
+} deriving (Eq)
 
 -- instaces
 instance Show Chess where
@@ -19,3 +26,19 @@ instance Show Game where
     show (Game slots) = "\n" ++ allLine 0 ++ allLine 1 ++ allLine 2 ++ allLine 3
         where   allLine n = "| " ++ line n ++ " | " ++ line (n + 4) ++ " | " ++ line (n + 8) ++ " | " ++ line (n + 12) ++ " |\n"
                 line n = intersperse ' ' . concat . map show . drop (n * 4) . take (n * 4 + 4) $ slots
+
+instance Show Stat where
+    show stat = "\n **** " ++ show (scoreFour stat) ++ 
+        "\n ***_ " ++ show (openThree stat) ++
+        "\n corn " ++ show (cornerAndCore stat) ++
+        "\n surf " ++ show (surface stat) ++
+        "\n"
+
+instance Monoid Stat where
+    mempty = Stat { scoreFour = 0, openThree = 0, cornerAndCore = 0, surface = 0 }
+    a `mappend` b = Stat {
+            scoreFour = scoreFour a + scoreFour b,
+            openThree = openThree a + openThree b,
+            cornerAndCore = cornerAndCore a + cornerAndCore b,
+            surface = surface a + surface b
+        }
