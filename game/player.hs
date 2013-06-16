d4e3efb8f5940bf65a6025d52c6680a602e6c7da
Ting-Yen LAI <banacorn@gmail.com>
2013-06-16 17:39:48 +0800

    foo

--- game/player.hs
+++ game/player.hs
@@ -1,3 +1,45 @@
 module Game.Player where
 
 import Game.Type
+
+n = Game $ replicate 64 Empty
+m = n =.= (0, 0)  =.= (0, 0)  =.= (0, 0)  =.= (0, 0) 
+
+
+action :: Player -> Game -> Game
+action _ game = game
+
+replace :: [a] -> Int -> a -> [a]
+replace [] n a = []
+replace (x:xs) 0 a = a:xs
+replace (x:xs) n a = x:replace xs (pred n) a
+
+dropChess chess position (Game slots) = Game $ dropChess' slots chess position
+    where   dropChess' [] chess (x, y) = []
+            dropChess' slots chess (x, y)
+                | firstLayer !! position == Empty = replace slots position chess
+                | otherwise                       = firstLayer ++ (dropChess' (drop 16 slots) chess (x, y))
+                where   firstLayer = take 16 slots
+                        position = x + 4 * y
+
+game =.= position = dropChess A position game
+game =-= position = dropChess B position game
+
+expand :: Game -> Chess -> [Game]
+expand game chess = map (\ position -> dropChess chess position game ) (availableSlot game)
+
+
+
+indexToPosition :: Int -> Position
+indexToPosition n = (mod n 4, div n 4)
+
+
+availableSlot :: Game -> ActionSequence
+availableSlot (Game slots) = toPosition 0 . drop 48 $ slots 
+    where   toPosition n []= []
+            toPosition n (Empty:xs) = indexToPosition n : toPosition (succ n) xs
+            toPosition n (_:xs) = toPosition (succ n) xs
+
+
+
+--evaluateAction :: Player -> Game -> (Int, Int)
--- game/type.hs
+++ game/type.hs
@@ -1,7 +1,13 @@
 module Game.Type where
+
+import Data.List (intersperse)
+
  --datatypes
 data Chess = Empty | A | B deriving (Eq)
+type Position = (Int, Int) 
+type ActionSequence = [Position]
 data Game = Game [Chess] deriving (Eq)
+data Player = Player deriving (Eq)
 
 -- instaces
 instance Show Chess where
@@ -11,5 +17,5 @@ instance Show Chess where
 
 instance Show Game where
     show (Game slots) = "\n" ++ allLine 0 ++ allLine 1 ++ allLine 2 ++ allLine 3
-        where   allLine n = line n ++ "|" ++ line (n + 4) ++ "|" ++ line (n + 8) ++ "|" ++ line (n + 12) ++ "\n"
-                line n = concat $ map show $ drop (n * 4) $ take (n * 4 + 4) $ slots
+        where   allLine n = "| " ++ line n ++ " | " ++ line (n + 4) ++ " | " ++ line (n + 8) ++ " | " ++ line (n + 12) ++ " |\n"
+                line n = intersperse ' ' . concat . map show . drop (n * 4) . take (n * 4 + 4) $ slots
